00c74b38dceacee11a3e7e27d6fafcb56c3f1369
KingVentrix007 <134402045+KingVentrix007@users.noreply.github.com>
2025-06-13 05:11:44 +0200

    minor fixes

--- httpe_client/httpe_client/httpe_client.py
+++ httpe_client/httpe_client/httpe_client.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone,timedelta
 import httpe_core.httpe_secure as sec  # Must have fernet_encrypt, fernet_decrypt, rsa_encrypt_key, encrypt_user_id
 import httpe_core.httpe_cert as httpe_cert           # Must have verify_cert(cert, host, pem_path, pubkey)
 import json
-import httpe_core.httpe_core.httpe_fernet as httpe_fernet
+import httpe_core.httpe_fernet as httpe_fernet
 import base64
 import threading
 from cryptography.hazmat.primitives import hashes
